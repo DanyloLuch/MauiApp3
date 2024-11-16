d1b18ce3f6ee3dc2476e8d30f71ee521019f42cb
Данило Кучерук < @DESKTOP-L6AD50V>
2024-11-16 13:12:30 +0200

    Add project files.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberEntry.Text, out int number))
+            {
+                DisplayAlert("Результат", $"Ви ввели число {number}", "OK");
+            }
+            else
+            {
+                DisplayAlert("Помилка", "Будь ласка, введіть ціле число.", "OK");
+            }
+        }
+    }
+
+}
